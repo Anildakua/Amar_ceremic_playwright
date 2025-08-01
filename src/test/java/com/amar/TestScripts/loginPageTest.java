aba374cd2f8dadd65a7cb436b99fc6159511c67f
Anil dakua <anildakua32@gmail.com>
2025-08-01 16:42:46 +0530

    complite the basic framework

--- src/test/java/com/amar/TestScripts/loginPageTest.java
+++ src/test/java/com/amar/TestScripts/loginPageTest.java
@@ -1,22 +1,26 @@
 package com.amar.TestScripts;
 
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.amar.constants.appConstans;
 import com.amar.utilityClasses.baseClass;
+import com.amar.utilityClasses.listnerClass;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
+@Listeners(listnerClass.class)
 public class loginPageTest extends baseClass {
 
 	@Test
 	public void login() {
 		
+		log.info("Landing into login page ");
 		//Entering user name and password
 		login.navigateToHomePage(prop.getProperty("user_name")
 				               , prop.getProperty("password"));
 		
 		assertThat(page).hasTitle(appConstans.HOME_PAGE_TITLE);//verifying the login is sucssefully log  
-		
+		log.info("successfully we loged in it to dashbord page");
 	}
 }
--- src/test/java/com/amar/utilityClasses/listnerClass.java
+++ src/test/java/com/amar/utilityClasses/listnerClass.java
@@ -4,43 +4,91 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import static com.amar.utilityClasses.playwrightFactory.screanShort;
+
 public class listnerClass extends baseClass implements ITestListener {
+
+	ExtentSparkReporter sparkReporter;
+	ExtentReports extent;
+	ExtentTest test;
+	String path;
 
 	@Override
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
 		ITestListener.super.onStart(context);
+		 System.out.println("******Scuite get stated*******");
+		String reportsPath = System.getProperty("user.dir") + "./ExtendRepots/report.html";
+		sparkReporter = new ExtentSparkReporter(reportsPath);
+		sparkReporter.config().setDocumentTitle("Amar-ceremic-Test");
+		sparkReporter.config().setReportName("End to end ");
+		sparkReporter.config().setTheme(Theme.DARK);
+
+		extent = new ExtentReports();
+		extent.attachReporter(sparkReporter);
+		extent.setSystemInfo("Application", "AmarCeramicTest");
+		extent.setSystemInfo("Enviroment", "QA");
+		extent.setSystemInfo("Username", System.getProperty("user.name"));
+
 	}
-	
+
 	@Override
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
-		ITestListener.super.onTestStart(result);
+		// ITestListener.super.onTestStart(result);
+		System.out.println(result.getMethod().getMethodName() + ": stated!");
+		test = extent.createTest(result.getMethod().getMethodName());
+		test.assignAuthor("Anil");
+		test.assignAuthor("regration");
+		test.assignDevice("windows");
 	}
 
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		// TODO Auto-generated method stub
-		ITestListener.super.onTestSuccess(result);
+		// ITestListener.super.onTestSuccess(result);
+		System.out.println(result.getMethod().getMethodName() + ": passed!");
+		extent.createTest(result.getMethod().getMethodName());
+		test.log(Status.PASS, "Test Case Pass :" + result.getMethod().getMethodName());
+
 	}
 
 	@Override
 	public void onTestFailure(ITestResult result) {
-		
-		
+		// System.out.println(result.getMethod().getMethodName()+": failed!");
+		test = extent.createTest(result.getMethod().getMethodName());
+		test.log(Status.FAIL, "Test Case Pass :" + result.getMethod().getMethodName());
+
+		path = System.getProperty("user.dir") + "./screanShots/" + result.getMethod().getMethodName()
+				+ System.currentTimeMillis() + ".png";
+		test.fail(result.getThrowable(), MediaEntityBuilder.createScreenCaptureFromPath(screanShort(path)).build());
+
 	}
 
 	@Override
 	public void onTestSkipped(ITestResult result) {
 		// TODO Auto-generated method stub
-		ITestListener.super.onTestSkipped(result);
-	}
+		// ITestListener.super.onTestSkipped(result);
+		System.out.println(result.getMethod().getMethodName() + ": skipped!");
+		test = extent.createTest(result.getMethod().getMethodName());
+		test.log(Status.SKIP, "Test Case Pass :" + result.getMethod().getMethodName());
 
+		test.fail(result.getThrowable(), MediaEntityBuilder.createScreenCaptureFromPath(screanShort(path)).build());
+
+	}
 
 	@Override
 	public void onFinish(ITestContext context) {
 		// TODO Auto-generated method stub
 		ITestListener.super.onFinish(context);
+		System.out.println("******Scuite get ended*******");
+		extent.flush();
 	}
 
 }
